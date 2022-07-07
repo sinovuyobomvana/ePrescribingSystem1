cd686ba7aea63e93a307b947d2cdd8f399818a62
Siyabonga Bekwa <siyabekwa03@gmail.com>
2022-07-07 23:17:56 +0200

    kslasla

--- EPrescribingSystem/Models/MedicationActiveIngredient.cs
+++ EPrescribingSystem/Models/MedicationActiveIngredient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,5 +14,15 @@ namespace EPrescribingSystem.Models
         public int MedicationActiveID { get; set; }
 
         public string IngredientStrength { get; set; }
+
+        [ForeignKey("ActiveIngredientID")]
+        public int ActiveIngredientID { get; set; }
+        public ActiveIngredient ActiveIngredient { get; set; }
+
+
+        [ForeignKey("MedicationID")]
+        public int MedicationID { get; set; }
+        public MedicationModel MedicationModel { get; set; }
+
     }
 }
