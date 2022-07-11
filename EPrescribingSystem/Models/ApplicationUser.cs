5c3315bde3be6cfe436bb9c19b697fc59747bc00
Analo Pika <s220027315@mandela.ac.za>
2022-07-11 19:10:17 +0200

    User Details added.

--- EPrescribingSystem/Models/ApplicationUser.cs
+++ EPrescribingSystem/Models/ApplicationUser.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +10,7 @@ namespace EPrescribingSystem.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Key]
         public string UserID { get; set; }
 
         public string FirstName { get; set; }
@@ -15,5 +18,23 @@ namespace EPrescribingSystem.Models
         public string LastName { get; set; }
 
         public byte[] ProfilePicture { get; set; }
+
+        public string Gender { get; set; }
+        [Required, EmailAddress]
+        public string EmailAddress { get; set; }
+
+        public string DOB { get; set; }
+        [Required, Phone]
+        public string ContactNum { get; set; }
+        public string RegistrationNum { get; set; }
+        public string PracticeNum { get; set; }
+        [ForeignKey("SuburbID")]
+        public string SuburbID { get; set; }
+        public SuburbModel SuburbModel { get; set; }
+
+        [ForeignKey("PharmacyID")]
+        public string PharmacyID { get; set; }
+        public PharmacyModel pharmacyModel { get; set; }
+
     }
 }
