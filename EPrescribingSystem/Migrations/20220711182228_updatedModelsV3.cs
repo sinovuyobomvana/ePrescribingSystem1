07c0991b9467d32d98a5cff24c3583140baceacd
Sinovuyo Bomvana <sinovuyobomvana2@gmail.com>
2022-07-11 20:33:42 +0200

    update

---
+++ EPrescribingSystem/Migrations/20220711182228_updatedModelsV3.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EPrescribingSystem.Migrations
+{
+    public partial class updatedModelsV3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "practiceNum",
+                table: "MedicalPractice",
+                newName: "PracticeID");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PracticeNumber",
+                table: "MedicalPractice",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PracticeNumber",
+                table: "MedicalPractice");
+
+            migrationBuilder.RenameColumn(
+                name: "PracticeID",
+                table: "MedicalPractice",
+                newName: "practiceNum");
+        }
+    }
+}
--- EPrescribingSystem/Models/ApplicationUser.cs
+++ EPrescribingSystem/Models/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,16 +10,52 @@ namespace EPrescribingSystem.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Key]
         public string UserID { get; set; }
 
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
 
+        public string IDNumber { get; set; }
+
         public byte[] ProfilePicture { get; set; }
 
+        public string Gender { get; set; }
+
+        [Required, EmailAddress]
+        public string EmailAddress { get; set; }
+
+        public string DateOfBirth { get; set; }
+
+        public string Addressine1 { get; set; }
+
+        public string AddressLine2 { get; set; }
+
+        public string PostalCode { get; set; }
+
+        //Doctor
+        public string HealthCouncilRegistrationNumber { get; set; }
+
+        public string HighestQualification { get; set; }
+
+
+        [Required, Phone]
+        public string ContactNumber { get; set; }
+        public string RegistrationNumber { get; set; }
+
+        //Pharmacist
+        public string PracticeNumber { get; set; }
+
         [ForeignKey("SuburbID")]
-        public int SuburbID { get; set; }
-        public Suburb SuburbModel { get; set; }
+        public string SuburbID { get; set; }
+
+        public SuburbModel SuburbModel { get; set; }
+
+        [ForeignKey("PharmacyID")]
+        public string PharmacyID { get; set; }
+
+        public PharmacyModel PharmacyModel { get; set; }
+
     }
 }
