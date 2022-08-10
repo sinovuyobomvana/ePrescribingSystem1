92598c46c701a41298a786d6476b417c0ebfe98d
Siyabonga Bekwa <siyabekwa03@gmail.com>
2022-08-10 20:10:54 +0200

    ready

---
+++ EPrescribingSystem/Areas/Admin/Data/Repository/IMedicationRepository.cs
@@ -0,0 +1,22 @@
+﻿using EPrescribingSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPrescribingSystem.Areas.Admin.Data.Repository
+{
+    public interface IMedicationRepository
+    {
+        Task<IEnumerable<Medication>> GetAllAsync();
+
+        Task<Medication> GetByIdAsync(int id);
+        Medication GetById(int id);
+        Task AddAsync(Medication medication);
+        Task<Medication> UpdateAsync(Medication medication);
+        //Task<pharmacy> UpdateAsync(int id, Pharmacy pharmacy);
+        Task DeleteAsync(int id);
+
+        Medication Delete(Medication medication);
+    }
+}
