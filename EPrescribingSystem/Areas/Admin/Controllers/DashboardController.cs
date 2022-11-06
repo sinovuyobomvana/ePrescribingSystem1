a3cc779fbc3719b32f1c058ae37467c0ce4840b1
Sinovuyo Bomvana <sinovuyobomvana2@gmail.com>
2022-11-06 02:10:21 +0200

    asdadd

---
+++ EPrescribingSystem/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EPrescribingSystem.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        [Route("[area]/[controller]/[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
