84bdcca24a68e1ff1df9f9abf5be79ee21078332
analopika <analopika@gmail.com>
2022-11-06 15:08:37 +0200

    mwdsds

--- EPrescribingSystem/Areas/Admin/Models/UserRolesViewModel.cs
+++ EPrescribingSystem/Areas/Admin/Models/UserRolesViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace EPrescribingSystem.Models
 {
@@ -10,6 +12,11 @@ namespace EPrescribingSystem.Models
         public string UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")]
+        [Display(Name = "Email address")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.(com|net|org|gov)$", ErrorMessage = "Invalid email address.")]
         public string Email { get; set; }
         public string SuburbName { get; set; }
         public IEnumerable<string> Roles { get; set; }
--- EPrescribingSystem/Areas/Admin/ViewModel/PharmacyViewModel.cs
+++ EPrescribingSystem/Areas/Admin/ViewModel/PharmacyViewModel.cs
@@ -10,7 +10,16 @@ namespace EPrescribingSystem.Areas.Admin.ViewModel
         public string Name { get; set; }
         public string AddressLine1 { get; set; }
         public string AddressLine2 { get; set; }
+
+        [Required(ErrorMessage = "Please enter contact no.")]
+        [RegularExpression("^((?:\\+27|27)|0)(=41|45|11|32|42|47)(\\d{7})$", ErrorMessage = "Phone number is not vallid")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "A valid phone number must have 10 digits.")]
         public string ContactNumber { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")]
+        [Display(Name = "Email address")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.(com|net|org|gov)$", ErrorMessage = "Invalid email address.")]
         public string EmailAddress { get; set; }
         public string PostalCode { get; set; }
         public string Province { get; set; }
